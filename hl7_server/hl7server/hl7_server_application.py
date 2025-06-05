f865045e44a0bf8b3dce15796e79b51ca0603043
Michal Fiedoruk <m.fiedoruk@kainos.com>
2025-06-05 12:14:03 +0200

    typing checks fixes

--- hl7_server/hl7server/hl7_server_application.py
+++ hl7_server/hl7server/hl7_server_application.py
@@ -2,42 +2,39 @@ import logging
 import os
 import signal
 import socket
+from typing import Any
 
 from hl7apy.mllp import MLLPServer
 
 from hl7_server.hl7server.generic_handler import GenericHandler
 
 # Configure logging
-log_level_str = os.environ.get('LOG_LEVEL', 'INFO').upper()
+log_level_str = os.environ.get("LOG_LEVEL", "INFO").upper()
 log_level = getattr(logging, log_level_str, logging.INFO)
-logging.basicConfig(level=log_level, format='%(asctime)s [%(levelname)s] %(message)s')
+logging.basicConfig(level=log_level, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
 
 
 class Hl7ServerApplication:
-
     def __init__(self) -> None:
-        self.HOST = os.environ.get('HOST')  # Default to all interfaces
-        self.PORT = int(os.environ.get('PORT'))  # Default HL7 MLLP port
+        self.HOST = os.environ.get("HOST", "127.0.0.1")
+        self.PORT = int(os.environ.get("PORT", "2575"))
 
         self.terminated = False
         signal.signal(signal.SIGINT, self._signal_handler)
         signal.signal(signal.SIGTERM, self._signal_handler)
 
-        self._server = None
+        self._server: MLLPServer = None
 
-    def _signal_handler(self, signum, frame)-> None:
+    def _signal_handler(self, signum: Any, frame: Any) -> None:
         logger.info("Shutdown signal received (signal %s).", signum)
         self.terminated = True
         if self._server:
             self._server.shutdown()
 
-
     def start_server(self) -> None:
-
         logger.info(f"MLLP Server listening on {self.HOST}:{self.PORT}")
-        handlers = {"ADT^A31^ADT_A05": (GenericHandler,),
-                    "ADT^A28^ADT_A05": (GenericHandler,)}
+        handlers = {"ADT^A31^ADT_A05": (GenericHandler,), "ADT^A28^ADT_A05": (GenericHandler,)}
 
         try:
             self._server = MLLPServer(self.HOST, self.PORT, handlers)
@@ -58,6 +55,6 @@ class Hl7ServerApplication:
             logger.info("HL7 MLLP server shut down.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = Hl7ServerApplication()
     app.start_server()
