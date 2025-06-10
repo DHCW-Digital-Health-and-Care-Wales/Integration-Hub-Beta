1d2f48ef5e43acee1e76b53f6b1fdf46b6b54f77
Ikram Bapu <ikrambapu@msn.com>
2025-06-10 15:03:50 +0100

    INSER-531175: Fix error handling
    - Add logging when unsupported message type or invalid hl7 message
    - Change to run server in a separate thread to allow graceful shutdown

---
+++ hl7_server/tests/test_error_handler.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import patch
+from hl7apy.mllp import UnsupportedMessageType, InvalidHL7Message
+
+from hl7_server.error_handler import ErrorHandler
+
+
+class TestErrorHandler(unittest.TestCase):
+
+    @patch('hl7_server.error_handler.logger')
+    def test_reply_with_unsupported_message_type(self, mock_logger):
+        exception = UnsupportedMessageType("Unsupported type")
+        unsupported_message = "MSH|^~\&|GHH_ADT||||20080115153000||ADT^A01^ADT_A01|0123456789|P|2.5||||AL"
+        handler = ErrorHandler(exc=exception, message=unsupported_message)
+
+        handler.reply()
+
+        mock_logger.error.assert_called_once_with(
+            "Unsupported Message Type: %s", exception
+        )
+
+    @patch('hl7_server.error_handler.logger')
+    def test_reply_with_invalid_hl7_message_exception(self, mock_logger):
+        exception = InvalidHL7Message("invalid message")
+        invalid_message = "<hello>"
+        handler = ErrorHandler(exc=exception, message=invalid_message)
+
+        handler.reply()
+
+        mock_logger.error.assert_called_once_with(
+            "Invalid HL7 Message: %s", exception
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
