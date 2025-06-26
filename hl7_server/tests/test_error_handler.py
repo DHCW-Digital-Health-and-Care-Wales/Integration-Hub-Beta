633dba1eabfabaffc259befa96891a911aecb056
Martyna Świerszcz <martyna.swierszcz@kainos.com>
2025-06-26 13:16:22 +0200

    added exception parameter

--- hl7_server/tests/test_error_handler.py
+++ hl7_server/tests/test_error_handler.py
@@ -13,14 +13,13 @@ class TestErrorHandler(unittest.TestCase):
         unsupported_message = r"MSH|^~\&|GHH_ADT||||20080115153000||ADT^A01^ADT_A01|0123456789|P|2.5||||AL"
         mock_audit_client = MagicMock()
 
-        handler = ErrorHandler(unsupported_message, mock_audit_client)
-        handler.exc = exception
+        handler = ErrorHandler(exception, unsupported_message, mock_audit_client)
         handler.incoming_message = unsupported_message
 
         with self.assertRaises(UnsupportedMessageType):
             handler.reply()
 
-        mock_logger.error.assert_called_once()
+        mock_logger.error.assert_called_once_with(f"Unsupported Message Type: {exception}")
         mock_audit_client.log_message_failed.assert_called_once_with(
             unsupported_message, f"Unsupported Message Type: {exception}", "Unsupported message type"
         )
@@ -31,14 +30,12 @@ class TestErrorHandler(unittest.TestCase):
         invalid_message = "<hello>"
         mock_audit_client = MagicMock()
 
-        handler = ErrorHandler(invalid_message, mock_audit_client)
-        handler.exc = exception
-        handler.incoming_message = invalid_message
+        handler = ErrorHandler(exception, invalid_message, mock_audit_client)
 
         with self.assertRaises(InvalidHL7Message):
             handler.reply()
 
-        mock_logger.error.assert_called_once()
+        mock_logger.error.assert_called_once_with(f"Invalid HL7 Message: {exception}")
         mock_audit_client.log_message_failed.assert_called_once_with(
             invalid_message, f"Invalid HL7 Message: {exception}", "Invalid HL7 message format"
         )
