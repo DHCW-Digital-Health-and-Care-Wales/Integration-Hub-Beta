04f9796495d1b2d169e952098b00cc4fc4e45919
Michal Fiedoruk <m.fiedoruk@kainos.com>
2025-07-04 10:55:29 +0200

    fix MyPy type check errors in hl7_server

--- hl7_server/tests/test_hl7_server_application.py
+++ hl7_server/tests/test_hl7_server_application.py
@@ -2,6 +2,7 @@ import os
 import signal
 import unittest
 from threading import Thread
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 from hl7apy.mllp import MLLPServer
@@ -24,36 +25,38 @@ ENV_VARS = {
 @patch("hl7_server.hl7_server_application.ServiceBusClientFactory")
 @patch("hl7_server.hl7_server_application.threading.Thread")
 class TestHl7ServerApplication(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.app = Hl7ServerApplication()
 
-    def _setup_mocks(self, mock_thread: Thread, mock_mllp_server: MLLPServer):
+    def _setup_mocks(self, mock_thread: Thread, mock_mllp_server: MLLPServer) -> tuple[MagicMock, MagicMock]:
         mock_server_instance = MagicMock()
         mock_thread_instance = MagicMock()
         mock_mllp_server.return_value = mock_server_instance
-        mock_thread.return_value = mock_thread_instance
+        mock_thread.return_value = mock_thread_instance  # type: ignore
         return mock_server_instance, mock_thread_instance
 
-    def _assert_shutdown(self, server, thread):
+    def _assert_shutdown(self, server: MagicMock, thread: MagicMock) -> None:
         server.shutdown.assert_called_once()
         server.server_close.assert_called_once()
         thread.join.assert_called_once()
 
-    def test_server_initialization_and_shutdown(self, mock_thread: Thread, _, mock_mllp_server: MLLPServer) -> None:
+    def test_server_initialization_and_shutdown(
+        self, mock_thread: Thread, _: Any, mock_mllp_server: MLLPServer
+    ) -> None:
         server, thread = self._setup_mocks(mock_thread, mock_mllp_server)
         self.app.start_server()
         self.app.stop_server()
 
         self._assert_shutdown(server, thread)
 
-    def test_signal_handler_shutdown(self, mock_thread: Thread, _, mock_mllp_server: MLLPServer) -> None:
+    def test_signal_handler_shutdown(self, mock_thread: Thread, _: Any, mock_mllp_server: MLLPServer) -> None:
         server, thread = self._setup_mocks(mock_thread, mock_mllp_server)
         self.app.start_server()
         self.app._signal_handler(signal.SIGINT, None)
 
         self._assert_shutdown(server, thread)
 
-    def test_server_exception_handling(self, mock_thread: Thread, _, mock_mllp_server: MLLPServer) -> None:
+    def test_server_exception_handling(self, mock_thread: Thread, _: Any, mock_mllp_server: MLLPServer) -> None:
         server, thread = self._setup_mocks(mock_thread, mock_mllp_server)
         thread.start.side_effect = RuntimeError("Simulated server error")
 
