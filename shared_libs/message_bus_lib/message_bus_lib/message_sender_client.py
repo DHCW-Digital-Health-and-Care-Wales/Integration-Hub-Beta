b9bc005936b178eaef3b3532b7949be2a056d1b9
Michal Fiedoruk <m.fiedoruk@kainos.com>
2025-07-09 11:05:51 +0200

    Retry call to send message to Azure Service Bus 3 times.
    As per Microsoft guidelines in intermittent errors calls to Azure Service Bus should be retried:
    
    https://learn.microsoft.com/en-us/python/api/overview/azure/servicebus-readme?view=azure-python

--- shared_libs/message_bus_lib/message_bus_lib/message_sender_client.py
+++ shared_libs/message_bus_lib/message_bus_lib/message_sender_client.py
@@ -1,9 +1,15 @@
 import logging
 from azure.servicebus import ServiceBusMessage, ServiceBusSender
+from azure.servicebus.exceptions import (
+    MessageSizeExceededError,
+    OperationTimeoutError,
+    ServiceBusError,
+)
 from typing import Dict, Optional
 
 logger = logging.getLogger(__name__)
 
+MAX_SERVICE_BUS_RETRIES = 3
 
 class MessageSenderClient:
     def __init__(self, sender: ServiceBusSender, message_destination: str):
@@ -16,8 +22,22 @@ class MessageSenderClient:
             application_properties=custom_properties or {}
         )
 
-        self.sender.send_messages(message)
-        logger.debug("Message sent successfully to: %s", self.message_destination)
+        last_error = None
+        for _ in range(MAX_SERVICE_BUS_RETRIES):
+            try:
+                self.sender.send_messages(message)
+                logger.debug("Message sent successfully to: %s", self.message_destination)
+                return
+            except OperationTimeoutError:
+                continue
+            except MessageSizeExceededError:
+                raise
+            except ServiceBusError as e:
+                last_error = e
+                continue
+        if last_error:
+            raise last_error
+        
 
     def send_text_message(self, message_text: str, custom_properties: Optional[Dict[str, str]] = None):
         self.send_message(message_text.encode('utf-8'), custom_properties)
@@ -28,6 +48,11 @@ class MessageSenderClient:
 
     def __enter__(self):
         return self
+
+    def close(self):
+        if self.sender:
+            self.sender.close()
+        logger.debug("ServiceBusSenderClient closed.")
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.close()
--- shared_libs/message_bus_lib/tests/test_MessageSenderClient.py
+++ shared_libs/message_bus_lib/tests/test_MessageSenderClient.py
@@ -2,6 +2,9 @@ import unittest
 from unittest.mock import MagicMock
 
 from message_bus_lib.message_sender_client import MessageSenderClient
+from azure.servicebus.exceptions import (
+    OperationTimeoutError,
+)
 
 
 class TestMessageSenderClient(unittest.TestCase):
@@ -13,13 +16,24 @@ class TestMessageSenderClient(unittest.TestCase):
 
     def test_send_message_sends_message_to_service_bus(self):
         # Arrange
-        message = "Test Message"
+        message = b"Test Message"
 
         # Act
         self.message_sender_client.send_message(message)
 
         # Assert
         self.service_bus_sender_client.send_messages.assert_called_once()
+
+    def test_send_message_retries_on_failure(self):
+        # Arrange
+        message = b"Test Message"
+        self.service_bus_sender_client.send_messages = MagicMock(side_effect=OperationTimeoutError())
+
+        # Act
+        self.message_sender_client.send_message(message)
+
+        # Assert
+        self.assertEqual(len(self.service_bus_sender_client.send_messages.mock_calls), 3)
 
     def test_exit_service_bus_sender_client_closed(self):
         # Arrange
