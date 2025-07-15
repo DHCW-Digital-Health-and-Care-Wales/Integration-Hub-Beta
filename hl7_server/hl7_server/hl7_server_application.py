7f2d3eda892752735ab0699f992ec80f65ebd00f
Chris Iliev <c.iliev@kainos.com>
2025-07-15 11:47:03 +0100

    feat: add paris a40^adt_a39 message type generic handler to registry

--- hl7_server/hl7_server/hl7_server_application.py
+++ hl7_server/hl7_server/hl7_server_application.py
@@ -56,7 +56,8 @@ class Hl7ServerApplication:
         handlers = {
             "ADT^A31^ADT_A05": (GenericHandler, self.sender_client, self.audit_client, self.validator),
             "ADT^A28^ADT_A05": (GenericHandler, self.sender_client, self.audit_client, self.validator),
-            "ADT^A40^ADT_A05": (GenericHandler, self.sender_client, self.audit_client, self.validator),
+            # Paris A40 message
+            "ADT^A40^ADT_A39": (GenericHandler, self.sender_client, self.audit_client, self.validator),
             # Chemocare messages
             "ADT^A31": (GenericHandler, self.sender_client, self.audit_client, self.validator),
             "ADT^A28": (GenericHandler, self.sender_client, self.audit_client, self.validator),
