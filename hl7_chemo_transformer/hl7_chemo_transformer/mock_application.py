480ede04e0966d5e5072b1450eb7325040c4bf08
Thomas-Boyle <thomasboyle@kainos.com>
2025-07-21 10:41:18 +0100

    wip: integration with poc_transformer message creation

--- hl7_chemo_transformer/hl7_chemo_transformer/mock_application.py
+++ hl7_chemo_transformer/hl7_chemo_transformer/mock_application.py
@@ -5,97 +5,118 @@ from hl7apy.parser import parse_message
 
 
 def transform_chemocare(hl7_msg: Message) -> Message:
-    original_er7 = hl7_msg.to_er7()
-    transformed_msg = parse_message(original_er7)
-    _transform_msh_segment(transformed_msg)
-    _transform_pid_segment(transformed_msg)
-
-    return transformed_msg
+    return create_new_message(hl7_msg)
 
 
-def _transform_msh_segment(hl7_msg: Message) -> None:
-    msh = hl7_msg.msh
+def create_new_message(original_message: Message) -> Message:
+    original_msh = original_message.msh
+    original_pid = original_message.pid
 
-    # MSH.9/MSG.3 = "ADT_A05"
-    if hasattr(msh, "msh_9") and msh.msh_9:
-        msh.msh_9.msh_9_3 = "ADT_A05"
+    new_message = Message(version="2.5")
 
-    # MSH.12/VID.1 = "2.5"
-    if hasattr(msh, "msh_12") and msh.msh_12:
-        msh.msh_12.msh_12_1 = "2.5"
+    new_message.msh.msh_1 = original_msh.msh_1
+    new_message.msh.msh_2 = original_msh.msh_2
 
-    # Copy MSH.20 to MSH.21/EI.1 and clear MSH.20
-    msh20_value = ""
-    if hasattr(msh, "msh_20") and msh.msh_20:
-        msh20_value = msh.msh_20.value
-        msh.msh_20 = ""
+    if hasattr(original_msh, "msh_3") and original_msh.msh_3.hd_1:
+        new_message.msh.msh_3.hd_1 = original_msh.msh_3.hd_1
 
-    if hasattr(msh, "msh_21") and msh.msh_21 and msh20_value:
-        msh.msh_21.msh_21_1 = msh20_value
+    if hasattr(original_msh, "msh_4") and original_msh.msh_4.hd_1:
+        new_message.msh.msh_4.hd_1 = original_msh.msh_4.hd_1
 
-    # Copy MSH.21 to MSH.21/EI.1
-    if hasattr(msh, "msh_21") and msh.msh_21:
-        msh21_value = msh.msh_21.value
-        msh.msh_21.msh_21_1 = msh21_value
-        msh.msh_21 = ""
+    if hasattr(original_msh, "msh_5") and original_msh.msh_5.hd_1:
+        new_message.msh.msh_5.hd_1 = original_msh.msh_5.hd_1
 
+    if hasattr(original_msh, "msh_6") and original_msh.msh_6.hd_1:
+        new_message.msh.msh_6.hd_1 = original_msh.msh_6.hd_1
 
-def _transform_pid_segment(hl7_msg: Message) -> None:
-    pid = hl7_msg.pid
-    msh = hl7_msg.msh
+    if hasattr(original_msh, "msh_7") and original_msh.msh_7.ts_1:
+        new_message.msh.msh_7.ts_1 = original_msh.msh_7.ts_1
 
-    pid2_value = ""
-    if hasattr(pid, "pid_2") and pid.pid_2:
-        pid2_value = pid.pid_2.pid_2_1.value
-        pid.pid_2.pid_2_1 = ""
+    if hasattr(original_msh, "msh_9") and original_msh.msh_9:
+        new_message.msh.msh_9.msg_1 = _safe_get_value(original_msh.msh_9, "msg_1") or _safe_get_value(
+            original_msh.msh_9, "msh_9_1"
+        )
+        new_message.msh.msh_9.msg_2 = _safe_get_value(original_msh.msh_9, "msg_2") or _safe_get_value(
+            original_msh.msh_9, "msh_9_2"
+        )
+        new_message.msh.msh_9.msg_3 = "ADT_A05"
 
-    msh3_value = ""
-    if hasattr(msh, "msh_3") and msh.msh_3:
-        msh3_value = msh.msh_3.msh_3_1.value if hasattr(msh.msh_3, "msh_3_1") else str(msh.msh_3.value)
+    if hasattr(original_msh, "msh_10") and original_msh.msh_10:
+        new_message.msh.msh_10 = original_msh.msh_10
 
-    # Update PID.3 first repetition
-    if hasattr(pid, "pid_3") and pid.pid_3 and pid2_value:
-        current_pid3_rep1 = pid.pid_3[0].value if pid.pid_3 else ""
-        components_rep1 = current_pid3_rep1.split("^")
+    if hasattr(original_msh, "msh_11") and original_msh.msh_11.pt_1:
+        new_message.msh.msh_11.pt_1 = original_msh.msh_11.pt_1
 
-        # Ensure we have components for first repetition
-        while len(components_rep1) < 5:
-            components_rep1.append("")
+    new_message.msh.msh_12.vid_1 = "2.5"
 
-        # Set CX.1 to the PID.2 value
-        components_rep1[0] = pid2_value
-        # Set CX.4 to "NHS"
-        components_rep1[3] = "NHS"
-        # Set CX.5 to "NH"
-        components_rep1[4] = "NH"
+    # Copy other MSH fields that might exist
+    for i in range(13, 20):
+        field_name = f"msh_{i}"
+        if hasattr(original_msh, field_name):
+            field_value = getattr(original_msh, field_name)
+            if field_value:
+                setattr(new_message.msh, field_name, field_value)
 
-        pid.pid_3[0].value = "^".join(components_rep1)
+    msh20_value = _safe_get_value(original_msh, "msh_20")
+    msh21_value = _safe_get_value(original_msh, "msh_21")
+    if msh20_value:
+        new_message.msh.msh_21.ei_1 = msh20_value
+    elif msh21_value:
+        new_message.msh.msh_21.ei_1 = msh21_value
 
-    # Update PID.3 second repetition
-    if hasattr(pid, "pid_3") and pid.pid_3 and len(pid.pid_3) > 1 and pid2_value and msh3_value:
-        current_pid3_rep2 = pid.pid_3[1].value if len(pid.pid_3) > 1 else ""
-        components_rep2 = current_pid3_rep2.split("^")
+    if hasattr(original_pid, "pid_1") and original_pid.pid_1:
+        new_message.pid.pid_1 = original_pid.pid_1
 
-        # Ensure we have components for second repetition
-        while len(components_rep2) < 5:
-            components_rep2.append("")
+    pid2_value = _safe_get_value(original_pid, "pid_2.pid_2_1") or _safe_get_value(original_pid, "pid_2")
+    msh3_value = _safe_get_value(original_msh, "msh_3.msh_3_1") or _safe_get_value(original_msh, "msh_3")
 
-        # Set CX.1 to PID.2 value + MSH.3.1 value
-        components_rep2[0] = pid2_value + msh3_value
-        # Set CX.4 to MSH.3.1 value directly as HD.1
-        components_rep2[3] = msh3_value
-        # Set CX.5 to PI
-        components_rep2[4] = "PI"
+    if pid2_value:
+        pid3_rep1 = new_message.pid.add_field("pid_3")
+        pid3_rep1.cx_1 = pid2_value
+        pid3_rep1.cx_2 = f"{pid2_value}{msh3_value}" if msh3_value else pid2_value
+        pid3_rep1.cx_4.hd_1 = "NHS"
+        pid3_rep1.cx_5 = "NH"
 
-        pid.pid_3[1].value = "^".join(components_rep2)
+    if msh3_value:
+        pid3_rep2 = new_message.pid.add_field("pid_3")
+        pid3_rep2.cx_1 = msh3_value
+        pid3_rep2.cx_4.hd_1 = "NHS"
+        pid3_rep2.cx_5 = "PI"
 
-    pid32_value = ""
-    if hasattr(pid, "pid_32") and pid.pid_32:
-        pid32_value = pid.pid_32.value
-        pid.pid_32 = ""
+    if hasattr(original_pid, "pid_5") and original_pid.pid_5:
+        new_message.pid.pid_5 = original_pid.pid_5
 
-    if hasattr(pid, "pid_31") and pid32_value:
-        pid.pid_31 = pid32_value
+    if hasattr(original_pid, "pid_7") and original_pid.pid_7:
+        new_message.pid.pid_7 = original_pid.pid_7
+
+    if hasattr(original_pid, "pid_8") and original_pid.pid_8:
+        new_message.pid.pid_8 = original_pid.pid_8
+
+    if hasattr(original_pid, "pid_11") and original_pid.pid_11:
+        new_message.pid.pid_11 = original_pid.pid_11
+
+    if hasattr(original_pid, "pid_13") and original_pid.pid_13:
+        new_message.pid.pid_13 = original_pid.pid_13
+
+    pid32_value = _safe_get_value(original_pid, "pid_32")
+    if pid32_value:
+        new_message.pid.pid_31 = pid32_value
+
+    if hasattr(original_message, "evn") and original_message.evn:
+        new_message.evn = original_message.evn
+
+    if hasattr(original_message, "pv1") and original_message.pv1:
+        new_message.pv1 = original_message.pv1
+
+    segment_names = ["pd1", "nk1", "pv2", "obx", "al1", "dg1", "pr1", "gt1", "in1", "in2", "in3"]
+
+    for segment_name in segment_names:
+        if hasattr(original_message, segment_name):
+            segment = getattr(original_message, segment_name)
+            if segment:
+                setattr(new_message, segment_name, segment)
+
+    return new_message
 
 
 # Used to access HL7 nested fields directly (without crashing)
