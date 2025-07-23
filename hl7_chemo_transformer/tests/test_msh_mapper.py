435bc66efbf5b6ceef3d733601a87b32446827b8
Thomas-Boyle <thomasboyle@kainos.com>
2025-07-23 13:22:18 +0100

    test: add msg and pid mapping tests

--- hl7_chemo_transformer/tests/test_msh_mapper.py
+++ hl7_chemo_transformer/tests/test_msh_mapper.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import MagicMock, patch
 
 from hl7apy.core import Message
 from hl7apy.parser import parse_message
@@ -15,55 +14,49 @@ class TestMSHMapper(unittest.TestCase):
 
         self.new_message = Message(version="2.5")
 
-    def test_map_msh_basic_fields(self) -> None:
+    def test_map_msh_all_transformed_mappings(self) -> None:
         map_msh(self.original_message, self.new_message)
 
-        self.assertEqual(
-            get_hl7_field_value(self.original_message.msh, "msh_1"),
-            get_hl7_field_value(self.new_message.msh, "msh_1"),
-        )
-        self.assertEqual(
-            get_hl7_field_value(self.original_message.msh, "msh_2"),
-            get_hl7_field_value(self.new_message.msh, "msh_2"),
-        )
+        test_cases = [
+            ("msh_9.msg_3", "ADT_A05"),
+            ("msh_9", "ADT^A28^ADT_A05"),
+            ("msh_12.vid_1", "2.5"),
+        ]
+        for field_path, expected_value in test_cases:
+            self.assertEqual(expected_value, get_hl7_field_value(self.new_message.msh, field_path))
 
-    def test_map_msh_nested_fields(self) -> None:
+    def test_map_msh_all_direct_mappings(self) -> None:
         map_msh(self.original_message, self.new_message)
 
-        self.assertEqual(
-            get_hl7_field_value(self.original_message.msh, "msh_3.hd_1"),
-            get_hl7_field_value(self.new_message.msh, "msh_3.hd_1"),
-        )
+        test_cases = [
+            "msh_1",
+            "msh_2",
+            "msh_3.hd_1",
+            "msh_4.hd_1",
+            "msh_5.hd_1",
+            "msh_6.hd_1",
+            "msh_7.ts_1",
+            "msh_8",
+            "msh_9.msg_1",
+            "msh_9.msg_2",
+            "msh_10",
+            "msh_11",
+            "msh_13",
+            "msh_14",
+            "msh_15",
+            "msh_16",
+            "msh_17",
+            "msh_18",
+            "msh_19",
+            "msh_20",
+            "msh_21",
+        ]
 
-    def test_map_msh_message_type(self) -> None:
-        map_msh(self.original_message, self.new_message)
-
-        self.assertEqual(
-            get_hl7_field_value(self.original_message.msh, "msh_9.msg_1"),
-            get_hl7_field_value(self.new_message.msh, "msh_9.msg_1"),
-        )
-        self.assertEqual(
-            get_hl7_field_value(self.original_message.msh, "msh_9.msg_2"),
-            get_hl7_field_value(self.new_message.msh, "msh_9.msg_2"),
-        )
-        self.assertEqual(
-            "ADT_A05",
-            get_hl7_field_value(self.new_message.msh, "msh_9.msg_3"),
-        )
-        self.assertEqual("ADT^A28^ADT_A05", get_hl7_field_value(self.new_message.msh, "msh_9"))
-
-    def test_map_msh_version_id(self) -> None:
-        map_msh(self.original_message, self.new_message)
-
-        self.assertEqual("2.5", get_hl7_field_value(self.new_message.msh, "msh_12.vid_1"))
-
-    @patch("hl7_chemo_transformer.mappers.msh_mapper.set_nested_field")
-    def test_map_msh_calls_set_nested_field(self, mock_set_nested_field: MagicMock) -> None:
-        map_msh(self.original_message, self.new_message)
-
-        # Verify that set_nested_field was called with the expected parameters
-        mock_set_nested_field.assert_any_call(self.original_message.msh, self.new_message.msh, "msh_3", "hd_1")
-        mock_set_nested_field.assert_any_call(self.original_message.msh, self.new_message.msh, "msh_9", "msg_1")
+        for field_path in test_cases:
+            self.assertEqual(
+                get_hl7_field_value(self.original_message.msh, field_path),
+                get_hl7_field_value(self.new_message.msh, field_path),
+            )
 
 
 if __name__ == "__main__":
--- hl7_chemo_transformer/tests/test_pid_mapper.py
+++ hl7_chemo_transformer/tests/test_pid_mapper.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import MagicMock, patch
 
 from hl7apy.core import Message
 from hl7apy.parser import parse_message
@@ -11,110 +10,79 @@ from hl7_chemo_transformer.utils.field_utils import get_hl7_field_value
 class TestPIDMapper(unittest.TestCase):
     def setUp(self) -> None:
         self.base_hl7_message = (
-            "MSH|^~\\&|192|192|200|200|20250701154910||ADT^A28|474997159036153|P|2.4|||NE|NE\r"
-            "PID|1|1000000001^^^^NH|1000001^^^^PAS~1000000001^^^^NH||TEST^TEST^TEST^^Mr.||20000101000000|M|||111, TEST^TEST^TEST^^CF11 9AD||01000 000001^PRN^^test@test.com~07000000001^PRS|07000000001^WPN||||||||||||||||||1\r"
+            "MSH|^~\\&|224|224|100|100|20250624162400||ADT^A31|109088442894725|P|2.4|||NE|NE\r"
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH~V100000A^^^^PAS||TEST^TEST^^^Ms.||20000101000000|F|||1 TEST^TEST^TEST^TEST^CF11 9AD||07000000001^PRN^^test@test.co.uk~07000000001^PRS~07000000001^PRN^^test@test.co.uk~|07000000001^WPN||||||||A||||||||||1\r"
         )
         self.original_message = parse_message(self.base_hl7_message)
         self.new_message = Message(version="2.5")
 
-    def _create_test_message(self, msh3_value: str = "192", pid32_value: str = "") -> Message:
-        pid_suffix = f"|||||||||||||||||||||||||||||||{pid32_value}" if pid32_value else "||||||||||||||||||1"
-        test_message = (
-            f"MSH|^~\\&|{msh3_value}|{msh3_value}|200|200|20250701154910||ADT^A28|474997159036153|P|2.4|||NE|NE\r"
-            f"PID|1|1000000001^^^^NH|1000001^^^^PAS~1000000001^^^^NH||TEST^TEST^TEST^^Mr.||20000101000000|M|||111, TEST^TEST^TEST^^CF11 9AD||01000 000001^PRN^^test@test.com~07000000001^PRS|07000000001^WPN{pid_suffix}\r"
-        )
-        return parse_message(test_message)
-
-    def test_map_pid_basic_fields(self) -> None:
-        """Test mapping of basic PID fields that should be copied directly."""
+    def test_map_pid_all_direct_mappings(self) -> None:
         map_pid(self.original_message, self.new_message)
 
-        basic_field_mappings = [
-            ("pid_1", "pid_1"),
-            ("pid_7", "pid_7"),
-            ("pid_8", "pid_8"),
-            ("pid_5.xpn_1.fn_1", "pid_5.xpn_1.fn_1"),
-            ("pid_11.xad_2", "pid_11.xad_2"),
-            ("pid_11.xad_3", "pid_11.xad_3"),
-            ("pid_13.xtn_1", "pid_13.xtn_1"),
-            ("pid_13.xtn_2", "pid_13.xtn_2"),
-            ("pid_14.xtn_1", "pid_14.xtn_1"),
-            ("pid_10.ce_1", "pid_10.ce_1"),
-            ("pid_17.ce_1", "pid_17.ce_1"),
+        test_cases = [
+            "pid_1",
+            "pid_5.xpn_1.fn_1",
+            "pid_5.xpn_2",
+            "pid_5.xpn_3",
+            "pid_5.xpn_4",
+            "pid_5.xpn_5",
+            "pid_5.xpn_6",
+            "pid_5.xpn_7",
+            "pid_5.xpn_8",
+            "pid_5.xpn_9.ce_1",
+            "pid_5.xpn_10.dr_1.ts_1",
+            "pid_5.xpn_10.dr_2.ts_1",
+            "pid_5.xpn_11",
+            "pid_6.xpn_1.fn_1",
+            "pid_7.ts_1",
+            "pid_8",
+            "pid_9.xpn_1.fn_1",
+            "pid_10.ce_1",
+            "pid_11.xad_1.sad_1",
+            "pid_11.xad_2",
+            "pid_11.xad_3",
+            "pid_11.xad_4",
+            "pid_11.xad_5",
+            "pid_11.xad_7",
+            "pid_11.xad_8",
+            "pid_13.xtn_1",
+            "pid_13.xtn_2",
+            "pid_14.xtn_1",
+            "pid_14.xtn_2",
+            "pid_17.ce_1",
+            "pid_22.ce_1",
+            "pid_29.ts_1",
         ]
 
-        for original_path, new_path in basic_field_mappings:
-            with self.subTest(field=original_path):
+        for field_path in test_cases:
+            with self.subTest(field=field_path):
                 self.assertEqual(
-                    get_hl7_field_value(self.original_message.pid, original_path),
-                    get_hl7_field_value(self.new_message.pid, new_path),
+                    get_hl7_field_value(self.original_message.pid, field_path),
+                    get_hl7_field_value(self.new_message.pid, field_path),
                 )
 
     def test_map_pid_patient_id_logic(self) -> None:
         map_pid(self.original_message, self.new_message)
 
-        self.assertEqual("NHS", get_hl7_field_value(self.new_message.pid, "pid_3.cx_4.hd_1"))
-        self.assertEqual("NH", get_hl7_field_value(self.new_message.pid, "pid_3.cx_5"))
-
-        expected_pid2_value = get_hl7_field_value(self.original_message.pid, "pid_2.cx_1") or get_hl7_field_value(
-            self.original_message.pid, "pid_2"
-        )
-        self.assertEqual(expected_pid2_value, get_hl7_field_value(self.new_message.pid, "pid_3.cx_1"))
+        self.assertEqual("NHS", get_hl7_field_value(self.new_message.pid.pid_3[0], "cx_4.hd_1"))
+        self.assertEqual("NH", get_hl7_field_value(self.new_message.pid.pid_3[0], "cx_5"))
+        self.assertEqual("PI", get_hl7_field_value(self.new_message.pid.pid_3[1], "cx_5"))
 
     def test_map_pid_health_board_logic(self) -> None:
-        health_board_cases = [
-            ("192", "SWW"),
-            ("224", "VCC"),
-            ("212", "BCUCCC"),
-            ("245", "SEW"),
-        ]
-
-        for msh3_value, expected_prefix in health_board_cases:
-            with self.subTest(msh3_value=msh3_value, expected_prefix=expected_prefix):
-                original_msg = self._create_test_message(msh3_value)
-                new_msg = Message(version="2.5")
-
-                map_pid(original_msg, new_msg)
-
-                expected_pid2_value = get_hl7_field_value(original_msg.pid, "pid_2.cx_1") or get_hl7_field_value(
-                    original_msg.pid, "pid_2"
-                )
-
-                if hasattr(new_msg.pid, "pid_3") and len(new_msg.pid.pid_3) > 1:
-                    second_rep_value = str(new_msg.pid.pid_3[1].cx_1.value) if new_msg.pid.pid_3[1].cx_1 else ""
-                    self.assertEqual(f"{expected_prefix}{expected_pid2_value}", second_rep_value)
-
-    def test_map_pid_32_to_31_mapping(self) -> None:
-        original_msg = self._create_test_message(pid32_value="TEST_IDENTITY")
-        new_msg = Message(version="2.5")
-
-        map_pid(original_msg, new_msg)
-
-        original_pid32_value = get_hl7_field_value(original_msg.pid, "pid_32")
-        if original_pid32_value:
-            self.assertEqual(original_pid32_value, get_hl7_field_value(new_msg.pid, "pid_31"))
-
-    def test_map_pid_no_pid_segment(self) -> None:
-        msh_only_message = "MSH|^~\\&|192|192|200|200|20250701154910||ADT^A28|474997159036153|P|2.4|||NE|NE\r"
-        original_msg = parse_message(msh_only_message)
-        new_msg = Message(version="2.5")
-
-        map_pid(original_msg, new_msg)
-
-    @patch("hl7_chemo_transformer.mappers.pid_mapper.set_nested_field")
-    def test_map_pid_calls_set_nested_field(self, mock_set_nested_field: MagicMock) -> None:
         map_pid(self.original_message, self.new_message)
 
-        expected_calls = [
-            ("pid_1",),
-            ("pid_5", "xpn_2"),
-            ("pid_7",),
-            ("pid_8",),
-        ]
+        original_pid2 = get_hl7_field_value(self.original_message.pid, "pid_2.cx_1")
+        new_pid3_rep2 = get_hl7_field_value(self.new_message.pid.pid_3[1], "cx_1")
 
-        for call_args in expected_calls:
-            with self.subTest(call_args=call_args):
-                mock_set_nested_field.assert_any_call(self.original_message.pid, self.new_message.pid, *call_args)
+        self.assertEqual(f"VCC{original_pid2}", new_pid3_rep2)
+
+    def test_map_pid_32_to_31_mapping(self) -> None:
+        map_pid(self.original_message, self.new_message)
+
+        self.assertEqual(
+            get_hl7_field_value(self.original_message.pid, "pid_32"),
+            get_hl7_field_value(self.new_message.pid, "pid_31"),
+        )
 
 
 if __name__ == "__main__":
