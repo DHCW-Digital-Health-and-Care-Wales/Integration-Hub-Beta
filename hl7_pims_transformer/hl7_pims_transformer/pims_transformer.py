70f6ec8bb351f8ec17e998d2e5496ef58c61b529
Chris Iliev <c.iliev@kainos.com>
2025-08-04 09:58:19 +0100

    feat: add pims transformer

--- hl7_pims_transformer/hl7_pims_transformer/pims_transformer.py
+++ hl7_pims_transformer/hl7_pims_transformer/pims_transformer.py
@@ -1,7 +1,15 @@
 from hl7apy.core import Message
 
+from .mappers.additional_segment_mapper import map_non_specific_segments
+from .mappers.evn_mapper import map_evn
+from .mappers.msh_mapper import map_msh
+
 
 def transform_pims_message(original_hl7_msg: Message) -> Message:
     new_message = Message(version="2.5")
+
+    map_msh(original_hl7_msg, new_message)
+    map_evn(original_hl7_msg, new_message)
+    map_non_specific_segments(original_hl7_msg, new_message)
 
     return new_message
