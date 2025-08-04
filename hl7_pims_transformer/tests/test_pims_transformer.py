6c5848b0d7e3396271ec64623e6f85b556947271
Chris Iliev <c.iliev@kainos.com>
2025-08-04 13:57:30 +0100

    test: add unit tests for pims transfomer

---
+++ hl7_pims_transformer/tests/test_pims_transformer.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import Mock, patch
+
+from hl7apy.parser import parse_message
+
+from hl7_pims_transformer.pims_transformer import transform_pims_message
+from tests.pims_messages import pims_messages
+
+
+class TestPimsTransformer(unittest.TestCase):
+    def test_transform_pims_message(self) -> None:
+        original_message = parse_message(pims_messages["a08"])
+
+        transformed_message = transform_pims_message(original_message)
+
+        self.assertEqual(transformed_message.version, "2.5")
+
+        segments_to_check = ["msh"]
+        for segment in segments_to_check:
+            self.assertTrue(hasattr(transformed_message, segment))
+
+        self.assertEqual(transformed_message.msh.msh_9.msg_1.value, "ADT")
+        self.assertEqual(transformed_message.msh.msh_10.value, "48209024")
+        self.assertEqual(transformed_message.msh.msh_11.value, "P")
+        self.assertEqual(transformed_message.msh.msh_17.value, "GBR")
+        self.assertEqual(transformed_message.msh.msh_19.ce_1.value, "EN")
+
+    @patch("hl7_pims_transformer.pims_transformer.map_msh")
+    @patch("hl7_pims_transformer.pims_transformer.map_evn")
+    @patch("hl7_pims_transformer.pims_transformer.map_non_specific_segments")
+    def test_all_mapper_functions_called(
+        self,
+        mock_map_evn: Mock,
+        mock_map_msh: Mock,
+        mock_map_non_specific: Mock,
+    ) -> None:
+        original_message = parse_message(pims_messages["a04"])
+
+        transformed_message = transform_pims_message(original_message)
+
+        mock_map_msh.assert_called_once_with(original_message, transformed_message)
+        mock_map_evn.assert_called_once_with(original_message, transformed_message)
+        mock_map_non_specific.assert_called_once_with(original_message, transformed_message)
