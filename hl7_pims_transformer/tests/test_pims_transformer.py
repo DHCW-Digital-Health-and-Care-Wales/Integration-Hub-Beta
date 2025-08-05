386eab58b1774d62f6e7cb5c2d6ff6f171a6766b
Chris Iliev <c.iliev@kainos.com>
2025-08-05 14:41:32 +0100

    test: add pid assertions to pims transformer unit test

--- hl7_pims_transformer/tests/test_pims_transformer.py
+++ hl7_pims_transformer/tests/test_pims_transformer.py
@@ -24,14 +24,25 @@ class TestPimsTransformer(unittest.TestCase):
         self.assertEqual(transformed_message.msh.msh_11.value, "P")
         self.assertEqual(transformed_message.msh.msh_17.value, "GBR")
         self.assertEqual(transformed_message.msh.msh_19.ce_1.value, "EN")
+        self.assertEqual(transformed_message.pid.pid_5.value, 'TESTER^TEST^""^^MRS.')
+        self.assertEqual(transformed_message.pid.pid_8.value, "F")
+        self.assertEqual(
+            transformed_message.pid.pid_11.value,
+            "MORRISTON HOSPITAL^HEOL MAES EGLWYS^CWMRHYDYCEIRW^SWANSEASWANSEA^SA6 6NL",
+        )
+        self.assertEqual(transformed_message.pid.pid_13[0].value, "01234567892")
+        self.assertEqual(transformed_message.pid.pid_13[1].value, "01234567896")
+        self.assertEqual(transformed_message.pid.pid_14.value, "")
 
     @patch("hl7_pims_transformer.pims_transformer.map_msh")
+    @patch("hl7_pims_transformer.pims_transformer.map_pid")
     @patch("hl7_pims_transformer.pims_transformer.map_evn")
     @patch("hl7_pims_transformer.pims_transformer.map_non_specific_segments")
     def test_all_mapper_functions_called(
         self,
         mock_map_evn: Mock,
         mock_map_msh: Mock,
+        mock_map_pid: Mock,
         mock_map_non_specific: Mock,
     ) -> None:
         original_message = parse_message(pims_messages["a04"])
@@ -39,5 +50,6 @@ class TestPimsTransformer(unittest.TestCase):
         transformed_message = transform_pims_message(original_message)
 
         mock_map_msh.assert_called_once_with(original_message, transformed_message)
+        mock_map_pid.assert_called_once_with(original_message, transformed_message)
         mock_map_evn.assert_called_once_with(original_message, transformed_message)
         mock_map_non_specific.assert_called_once_with(original_message, transformed_message)
