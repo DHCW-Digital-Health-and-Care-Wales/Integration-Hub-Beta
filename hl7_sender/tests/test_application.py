9aa7b734989470fdf7f31412254fd0a1b172abbb
Martyna Świerszcz <martyna.swierszcz@kainos.com>
2025-08-06 18:42:19 +0200

    ruff fix

--- hl7_sender/tests/test_application.py
+++ hl7_sender/tests/test_application.py
@@ -75,10 +75,10 @@ class TestProcessMessage(unittest.TestCase):
         mock_health_check_ctx.__enter__.return_value = mock_health_server
         mock_health_check.return_value = mock_health_check_ctx
         mock_app_config.read_env_config.return_value = AppConfig(
-            connection_string=None, 
-            ingress_queue_name="test-queue-name", 
-            service_bus_namespace=None, 
-            receiver_mllp_hostname="test-hostname", 
+            connection_string=None,
+            ingress_queue_name="test-queue-name",
+            service_bus_namespace=None,
+            receiver_mllp_hostname="test-hostname",
             receiver_mllp_port=2575,
             health_check_hostname="localhost",
             health_check_port=9000,
