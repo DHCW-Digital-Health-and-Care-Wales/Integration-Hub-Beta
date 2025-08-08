e9cb0903728db453ff6e5477f5c6b76a12396289
Chris Iliev <c.iliev@kainos.com>
2025-08-08 14:08:22 +0100

    feat: add pd1 mapper and tests

---
+++ hl7_pims_transformer/hl7_pims_transformer/mappers/pd1_mapper.py
@@ -0,0 +1,16 @@
+from hl7apy.core import Message
+
+from ..utils.field_utils import get_hl7_field_value, set_nested_field
+
+
+def map_pd1(original_hl7_message: Message, new_message: Message) -> None:
+    original_pd1 = getattr(original_hl7_message, "pd1", None)
+    if original_pd1 is None:
+        return  # No PD1 segment
+
+    original_message_type_trigger_event = original_hl7_message.msh.msh_9.msg_2.value
+    if original_message_type_trigger_event in ["A04", "A08"]:
+        set_nested_field(original_pd1, new_message.pd1, "pd1_4.xcn_1")
+
+        if len(getattr(original_pd1, "pd1_4", [])) > 1:
+            new_message.pd1.pd1_3.xon_3 = get_hl7_field_value(original_pd1, "pd1_4[1].xcn_1")
