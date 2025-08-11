63a7cf19f5d78d053924ad4b9323cd41009c64c4
Chris Iliev <c.iliev@kainos.com>
2025-08-11 11:44:20 +0100

    feat: add mrg mapper and tests

--- hl7_pims_transformer/hl7_pims_transformer/pims_transformer.py
+++ hl7_pims_transformer/hl7_pims_transformer/pims_transformer.py
@@ -2,6 +2,7 @@ from hl7apy.core import Message
 
 from .mappers.additional_segment_mapper import map_non_specific_segments
 from .mappers.evn_mapper import map_evn
+from .mappers.mrg_mapper import map_mrg
 from .mappers.msh_mapper import map_msh
 from .mappers.pd1_mapper import map_pd1
 from .mappers.pid_mapper import map_pid
@@ -16,6 +17,7 @@ def transform_pims_message(original_hl7_msg: Message) -> Message:
     map_pid(original_hl7_msg, new_message)
     map_pd1(original_hl7_msg, new_message)
     map_pv1(original_hl7_msg, new_message)
+    map_mrg(original_hl7_msg, new_message)
 
     map_non_specific_segments(original_hl7_msg, new_message)
 
