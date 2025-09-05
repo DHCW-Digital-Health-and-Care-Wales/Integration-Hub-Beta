6da976f9104a7a871cb80aa46d38c68dbe0c827a
Thomas-Boyle <thomasboyle@kainos.com>
2025-09-05 12:50:48 +0100

    feat(tests): seperate unit test files for chemo and phw HL7 message validation

---
+++ shared_libs/hl7_validation/tests/test_chemo.py
@@ -0,0 +1,84 @@
+import unittest
+
+from hl7_validation import validate_er7_with_flow
+from hl7_validation.validate import XmlValidationError
+
+
+class TestChemoMessages(unittest.TestCase):
+    def test_chemo_a31_schema_validation_successful(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|212|212|200|200|20250701140735||ADT^A31|201600952808665|P|2.4|||NE|"
+            "NE EVN|Sub|20250701140735",
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH||TEST^TEST^T^^Mrs.||20000101000000|F|||"
+            "TEST,^TEST^TEST TEST^^CF11 9AD||01000 000 001|07000000001||||||||||||||||||0",
+            "PD1||||G7000001",
+            "PV1||U"
+        ])
+
+        validate_er7_with_flow(er7, "chemo")
+
+    def test_chemo_a28_schema_validation_successful(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|212|212|200|200|20250701153930||ADT^A28|547417054344058|P|2.4|||NE|NE",
+            "EVN|Sub|20250701153930",
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH||TEST^TEST^^^Mr.||20000101000000|M|||"
+            "1, TEST^TEST^TEST^^CF11 9AD||01000 000001|||||||||||||||||||0",
+            "PD1||||G0000001",
+            "PV1||U"
+        ])
+
+        validate_er7_with_flow(er7, "chemo")
+
+    def test_chemo_a31_incorrectly_structured_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|212|212|200|200|20250701140735||ADT^A31|201600952808665|P|2.4|||NE|"
+            "NE EVN|Sub|20250701140735",
+            "XXX|This is an invalid segment not in the schema",  # Invalid segment
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH||TEST^TEST^T^^Mrs.||20000101000000|F|||"
+            "TEST,^TEST^TEST TEST^^CF11 9AD||01000 000 001|07000000001||||||||||||||||||0",
+            "PD1||||G7000001",
+            "PV1||U"
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "chemo")
+
+    def test_chemo_a28_incorrectly_structured_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|212|212|200|200|20250701153930||ADT^A28|547417054344058|P|2.4|||NE|NE",
+            "EVN|Sub|20250701153930",
+            "XXX|This is an invalid segment not in the schema",  # Invalid segment
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH||TEST^TEST^^^Mr.||20000101000000|M|||"
+            "1, TEST^TEST^TEST^^CF11 9AD||01000 000001|||||||||||||||||||0",
+            "PD1||||G0000001",
+            "PV1||U"
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "chemo")
+
+    def test_chemo_a31_correctly_structured_invalid_data_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|212|212|200|200|20250701140735||ADT^A31|201600952808665|P|2.4|||NE|"
+            "NE EVN|Sub|20250701140735",
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH||TEST^TEST^T^^Mrs.||20000101000000|F|||"
+            "TEST,^TEST^TEST TEST^^CF11 9AD||01000 000 001|07000000001||||||||||||||||||0",
+            "PD1||||G7000001"
+            # Missing PV1 segment which is required according to ADT_A31 schema
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "chemo")
+
+    def test_chemo_a28_correctly_structured_invalid_data_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|212|212|200|200|20250701153930||ADT^A28|547417054344058|P|2.4|||NE|NE",
+            "EVN|Sub|20250701153930",
+            "PID|1|1000000001^^^^NH|1000000001^^^^NH||TEST^TEST^^^Mr.||20000101000000|M|||"
+            "1, TEST^TEST^TEST^^CF11 9AD||01000 000001|||||||||||||||||||0",
+            "PD1||||G0000001"
+            # Missing PV1 segment which is required according to ADT_A28 schema
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "chemo")
---
+++ shared_libs/hl7_validation/tests/test_phw.py
@@ -0,0 +1,83 @@
+import unittest
+
+from hl7_validation import validate_er7_with_flow
+from hl7_validation.validate import XmlValidationError
+
+
+class TestPhwMessages(unittest.TestCase):
+    def test_phw_a05_schema_validation_successful(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|252|252|100|100|2025-05-05 23:23:30||ADT^A31^ADT_A05|"
+            "202505052323300000000000|P|2.5|||||GBR||EN",
+            "EVN|A05|20250502092900|20250505232330|||20250505232330",
+            "PID|||8888888^^^252^PI~4444444444^^^NHS^NH||SURNAME^FORENAME",
+            "PV1||"
+        ])
+
+        validate_er7_with_flow(er7, "phw")
+
+    def test_phw_a39_schema_validation_successful(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|252|252|100|100|2025-05-05 23:23:32||ADT^A28^ADT_A39||P|2.5|||||GBR||EN",
+            "EVN|A39|20250502092900|20250505232332|||20250505232332",
+            "PID|||8888888^^^252^PI~4444444444^^^NHS^NH||MYSURNAME^MYFNAME^MYMNAME^^MR||19990101|M|^^||"
+            "99, MY ROAD^MY PLACE^MY CITY^MY COUNTY^SA99 1XX^^H~^^^^^^||^^^~|||||||||||||||||||01",
+            "PD1|||^^W00000^|G999999",
+            "MRG|||7777777^^^252^PI~5555555555^^^NHS^NH",
+            "PV1||"
+        ])
+
+        validate_er7_with_flow(er7, "phw")
+
+    def test_phw_a05_incorrectly_structured_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|252|252|100|100|2025-05-05 23:23:30||ADT^A31^ADT_A05|"
+            "202505052323300000000000|P|2.5|||||GBR||EN",
+            "EVN|A05|20250502092900|20250505232330|||20250505232330",
+            "XXX|This is an invalid segment not in the schema",  # Invalid segment
+            "PID|||8888888^^^252^PI~4444444444^^^NHS^NH||SURNAME^FORENAME||19990101|M",
+            "PV1||"
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "phw")
+
+    def test_phw_a39_incorrectly_structured_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|252|252|100|100|2025-05-05 23:23:32||ADT^A28^ADT_A39||P|2.5|||||GBR||EN",
+            "EVN|A39|20250502092900|20250505232332|||20250505232332",
+            "XXX|This is an invalid segment not in the schema",  # Invalid segment
+            "PID|||8888888^^^252^PI~4444444444^^^NHS^NH||MYSURNAME^MYFNAME^MYMNAME^^MR||19990101|M|^^||"
+            "99, MY ROAD^MY PLACE^MY CITY^MY COUNTY^SA99 1XX^^H~^^^^^^||^^^~|||||||||||||||||||01",
+            "PD1|||^^W00000^|G999999",
+            "MRG|||7777777^^^252^PI~5555555555^^^NHS^NH",
+            "PV1||"
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "phw")
+
+    def test_phw_a05_correctly_structured_invalid_data_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|252|252|100|100|2025-05-05 23:23:30||ADT^A31^ADT_A05|"
+            "202505052323300000000000|P|2.5|||||GBR||EN",
+            "EVN|A05|20250502092900|20250505232330|||20250505232330",
+            "PID|||8888888^^^252^PI~4444444444^^^NHS^NH||SURNAME^FORENAME"
+            # Missing PV1 segment which is required according to ADT_A05 schema
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "phw")
+
+    def test_phw_a39_correctly_structured_invalid_data_validation_failure(self) -> None:
+        er7 = "\r".join([
+            "MSH|^~\\&|252|252|100|100|2025-05-05 23:23:32||ADT^A28^ADT_A39||P|2.5|||||GBR||EN",
+            "EVN|A39|20250502092900|20250505232332|||20250505232332",
+            "PID|||8888888^^^252^PI~4444444444^^^NHS^NH||MYSURNAME^MYFNAME^MYMNAME^^MR||19990101|M|^^||"
+            "99, MY ROAD^MY PLACE^MY CITY^MY COUNTY^SA99 1XX^^H~^^^^^^||^^^~|||||||||||||||||||01",
+            "PD1|||^^W00000^|G999999"
+            # Missing MRG segment which is required according to ADT_A39 schema
+        ])
+
+        with self.assertRaises(XmlValidationError):
+            validate_er7_with_flow(er7, "phw")
