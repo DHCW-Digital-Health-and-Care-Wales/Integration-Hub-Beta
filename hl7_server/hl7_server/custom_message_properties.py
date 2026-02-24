9a514920f1c52c309e15d15d0d2862ee1bed3bd0
Alex Richards <66647589+AlexJRichards@users.noreply.github.com>
2026-02-24 14:56:27 +0000

    Changed message properties to use multiple codes for 4.1

--- hl7_server/hl7_server/custom_message_properties.py
+++ hl7_server/hl7_server/custom_message_properties.py
@@ -24,14 +24,48 @@ def build_common_properties(workflow_id: str, msg_sending_app: str | None) -> di
 
 
 def build_mpi_properties(msg: Message) -> dict[str, str]:
+    pid3_codes = _get_pid3_4_1_codes(msg)
+    assigning_authorities = f"|{'|'.join(pid3_codes)}|" if pid3_codes else ""
+
+    pid2_codes = _get_pid2_4_1_codes(msg)
+    update_sources = f"|{'|'.join(pid2_codes)}|" if pid2_codes else ""
+
+
     return {
         "MessageType": get_hl7_field_value(msg, "msh.msh_9.msh_9_2"),
         "UpdateSource": get_hl7_field_value(msg, "pid.pid_2.cx_4.hd_1"),
+        "UpdateSources": update_sources,
         "AssigningAuthority": get_hl7_field_value(msg, "pid.pid_3.cx_4.hd_1"),
+        "AssigningAuthorities": assigning_authorities,
         "DateDeath": get_hl7_field_value(msg, "pid.pid_29.ts_1"),
         "ReasonDeath": get_hl7_field_value(msg, "pid.pid_30"),
     }
 
+def _get_pid3_4_1_codes(msg: Message) -> list[str]:
+    pid = getattr(msg, "pid", None)
+    pid3 = getattr(pid, "pid_3", None) if pid else None
+
+    codes = []
+
+    for code in pid3:
+        aa_code = (code.cx_4.hd_1.value.value or "").strip()
+        if aa_code not in codes:
+            codes.append(aa_code)
+
+    return codes
+
+def _get_pid2_4_1_codes(msg: Message) -> list[str]:
+    pid = getattr(msg, "pid", None)
+    pid2 = getattr(pid, "pid_2", None) if pid else None
+
+    codes = []
+
+    for code in pid2:
+        aa_code = (code.cx_4.hd_1.value.value or "").strip()
+        if aa_code not in codes:
+            codes.append(aa_code)
+
+    return codes
 
 FLOW_PROPERTY_BUILDERS: dict[str, FlowPropertyBuilder] = {
     "mpi": build_mpi_properties,
