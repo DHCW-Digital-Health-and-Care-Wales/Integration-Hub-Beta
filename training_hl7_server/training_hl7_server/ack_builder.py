50ce91fd9a683185b922b5375eae0a00a9190b76
Darian Govender <darian.govender@wales.nhs.uk>
2026-03-12 15:52:19 +0000

    Completed Week 1 Excercise

---
+++ training_hl7_server/training_hl7_server/ack_builder.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from hl7apy.consts import VALIDATION_LEVEL
+from hl7apy.core import Message, Segment
+
+from .constants import HL7Constants
+
+
+class AckBuilder:
+    def build_ack(self, message_control_id, original_msg, ack_code, error_message: str | None = None) -> Message:
+        ack = Message("ACK", validation_level=VALIDATION_LEVEL.STRICT)
+
+        # Build MSH segment
+        ack.msh.msh_1 = HL7Constants.FIELD_SEPARATOR
+        ack.msh.msh_2 = HL7Constants.ENCODING_CHARACTERS
+        ack.msh.msh_3 = original_msg.msh.msh_5.value
+        ack.msh.msh_4 = original_msg.msh.msh_6.value
+        ack.msh.msh_5 = original_msg.msh.msh_3.value
+        ack.msh.msh_6 = original_msg.msh.msh_4.value
+        ack.msh.msh_7 = datetime.now().strftime("%Y%m%d%H%M%S")
+        ack.msh.msh_9.message_code = HL7Constants.ACK_MESSAGE_TYPE_FORMAT
+        ack.msh.msh_9.trigger_event = original_msg.msh.msh_9.trigger_event.value
+        ack.msh.msh_10 = message_control_id
+        ack.msh.msh_11 = HL7Constants.PROCESSING_ID_PRODUCTION
+        ack.msh.msh_12 = original_msg.msh.msh_12.value
+
+        # Build MSA segment
+        msa = Segment("MSA", validation_level=VALIDATION_LEVEL.STRICT)
+        msa.msa_1 = ack_code
+        msa.msa_2 = message_control_id
+
+        if error_message:
+            msa.msa_3 = error_message
+
+        ack.add(msa)
+
+        return ack
